ed738eba2fa19de929832e000dc0f2ff9ad31095
Drew Bollinger <drew@developmentseed.org>
2018-01-18 10:02:52 -0500

    Add correct image sums

--- test/verify-labels-sg.py
+++ test/verify-labels-sg.py
@@ -1,31 +1,23 @@
 """Validate that the output produced by integration testing on 'label-maker labels'
 matches our expectations, specifically for segmentation tasks"""
-import json
 import numpy as np
 
 # our labels should look like this
-expected_labels = {
-    '62092-50162-17': np.array([1, 0, 0, 0, 0, 0, 0]),
-    '62092-50163-17': np.array([0, 0, 0, 0, 0, 0, 1]),
-    '62092-50164-17': np.array([0, 0, 0, 0, 0, 0, 1]),
-    '62093-50162-17': np.array([0, 0, 0, 0, 0, 0, 1]),
-    '62093-50164-17': np.array([0, 0, 0, 0, 0, 0, 1]),
-    '62094-50162-17': np.array([0, 0, 0, 0, 0, 0, 1]),
-    '62094-50164-17': np.array([0, 0, 0, 0, 0, 0, 1]),
-    '62094-50163-17': np.array([0, 1, 1, 0, 0, 0, 1]),
-    '62093-50163-17': np.array([0, 0, 0, 0, 1, 1, 1])
+expected_sums = {
+    '62092-50162-17': 0,
+    '62092-50163-17': 2760,
+    '62092-50164-17': 13728,
+    '62093-50162-17': 38004,
+    '62093-50164-17': 2688,
+    '62094-50162-17': 21630,
+    '62094-50164-17': 19440,
+    '62094-50163-17': 21895,
+    '62093-50163-17': 32198
 }
 
-labels = np.load('integration/labels.npz')
+labels = np.load('integration-sg/labels.npz')
 for tile in labels.files:
-    assert np.array_equal(expected_labels[tile], labels[tile])
-
-# our GeoJSON looks like the fixture
-expected_geojson = json.load(open('test/fixtures/integration/classification.geojson'))
-geojson = json.load(open('integration/classification.geojson'))
-
-for feature in geojson['features']:
-    assert feature in expected_geojson['features']
+    assert expected_sums[tile] == np.sum(labels[tile])
 
 # our command line output should look like this
 expected_output = """Determining labels for each tile
@@ -37,7 +29,15 @@ Ruins: 1 tiles
 Parking: 1 tiles
 Roads: 8 tiles
 Total tiles: 9
-Write out labels to integration/labels.npz
+Write out labels to integration-sg/labels.npz
+Writing 62092-50163-17.png
+Writing 62092-50164-17.png
+Writing 62093-50164-17.png
+Writing 62094-50162-17.png
+Writing 62093-50162-17.png
+Writing 62094-50164-17.png
+Writing 62094-50163-17.png
+Writing 62093-50163-17.png
 """
 
 with open('stdout', 'r') as output:
