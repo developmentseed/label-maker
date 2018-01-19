5e771b2445a2571c9d8a2e5ac4914d346c71483f
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2018-01-19 15:02:21 -0500

    AssertionError fix

--- test/verify-package-od.py
+++ test/verify-package-od.py
@@ -9,5 +9,5 @@ assert data['x_train'].shape == (6, 256, 256, 3)
 assert data['x_test'].shape == (2, 256, 256, 3)
 
 # validate our label data with exact matches in shape
-assert  data['y_train'].shape == (6, 16, 5)
+assert data['y_train'].shape == (6, 16, 5)
 assert data['y_test'].shape ==  (2, 16, 5)
