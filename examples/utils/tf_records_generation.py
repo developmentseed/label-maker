2edd1fe70a43b5b646662ef6603f579275141fae
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2018-01-30 11:03:45 -0500

    tf records generation

--- examples/utils/tf_records_generation.py
+++ examples/utils/tf_records_generation.py
@@ -4,10 +4,12 @@ This code was modified on top of Google tensorflow
 
 To use this code with Label Maker and Tensor Flow object detection API, this code work similar to  `label-maker package`.
 To create a correct training data set for Tensor Flow Object Detecrtion, we recommend you do:
+
 1. After running `label-maker images`, do `git clone https://github.com/tensorflow/models.git`
 2. Install TensorFlow object detection by following this: https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md
-3. From your Label Maker, copy `tiles` folder, this code `tf_records_generation.py` and `labels.npz` to Tensorflow object detecrtion directory `tensorflow/models/research/`
+3. From your Label Maker, copy `tiles` folder, this code `tf_records_generation.py` and `labels.py` to Tensorflow object detecrtion directory
 4. From directory `tensorflow/models/research/` simply runs:
+
 python3 tf_records_generation.py --label_input=labels.npz \
              --train_rd_path=data/train_buildings.record \
              --test_rd_path=data/test_buildings.record
@@ -19,7 +21,9 @@ import numpy as np
 from os import makedirs, path as op
 import shutil
 
+import pandas as pd
 import tensorflow as tf
+
 from PIL import Image
 from utils import dataset_util
 from collections import namedtuple
@@ -34,7 +38,6 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    """read in label data"""
     if row_label == 'building':
         return 1
     return None
@@ -94,7 +97,6 @@ Returns:
 
 
 def main(_):
-    """coverts numpy array into tfrecord files, the numpy array is in order of  'filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax'"""
     labels = np.load(op.join(os.getcwd(), FLAGS.label_input))
     tile_names = [tile for tile in labels.files]
     tile_names.sort()
@@ -121,12 +123,18 @@ def main(_):
                     y = ["{}.jpg".format(tile), width, height, cl_str, bbox[0], bbox[1], bbox[2], bbox[3]]
                     tf_tiles_info.append(y)
     #train_len = 0.8
-    tf_array = np.array(tf_tiles_info)
     split_index = int(len(tf_tiles_info) *0.8 )
-    tf_train = tf_array[0: split_index]
-    tf_test = tf_array[split_index, :]
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    df = pd.DataFrame(tf_tiles_info, columns=column_name)
+    #shuffle the dataframe
+    df = df.sample(frac=1)
+    train_df = df[:split_index]
+    test_df = df[split_index:]
     print("You have {} training tiles and {} test tiles ready".format(
-    len(set(list(tf_train[:,1]))), len(set(list(tf_test[:,1])))))
+    len(set(train_df['filename'])), len(set(test_df['filename']))))
+    # train_df.to_csv("train_df.csv")
+    # test_df.to_csv("test_df.csv")
+
     tiles_dir = op.join(os.getcwd(), 'tiles')
     train_dir = op.join(os.getcwd(), 'images', 'train')
     test_dir = op.join(os.getcwd(), 'images', 'test')
@@ -136,27 +144,31 @@ def main(_):
     if not op.isdir(test_dir):
         makedirs(test_dir)
 
-    for tile in list(tf_train[:,1]):
+    for tile in train_df['filename']:
         tile_dir = op.join(tiles_dir, tile)
         shutil.copy(tile_dir, train_dir)
 
-    # for tile in test_df['filename']:
-    for tile in list(tf_test[:,1]):
+    for tile in test_df['filename']:
         tile_dir = op.join(tiles_dir, tile)
         shutil.copy(tile_dir, test_dir)
     ### for train
     writer = tf.python_io.TFRecordWriter(FLAGS.train_rd_path)
-    tf_example = create_tf_example(tf_train, train_dir)
-    writer.write(tf_example.SerializeToString())
+    grouped = split(train_df, 'filename')
+
+    for group in grouped:
+        tf_example = create_tf_example(group, train_dir)
+        writer.write(tf_example.SerializeToString())
     writer.close()
     output_train= op.join(os.getcwd(),FLAGS.train_rd_path)
     print('Successfully created the TFRecords: {}'.format(output_train))
 
     ### for test
     writer = tf.python_io.TFRecordWriter(FLAGS.test_rd_path)
+    grouped = split(test_df, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, test_dir)
+        writer.write(tf_example.SerializeToString())
 
-    tf_example = create_tf_example(tf_test, test_dir)
-    writer.write(tf_example.SerializeToString())
     writer.close()
     output_test = op.join(os.getcwd(),FLAGS.test_rd_path)
     print('Successfully created the TFRecords: {}'.format(output_test))
