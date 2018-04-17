8fe8c02782dc0e2e325958f9702bc58ce671c274
Drew Bollinger <drew@developmentseed.org>
2018-04-16 22:58:09 -0400

    Add colors to segmentation labels

--- label_maker/label.py
+++ label_maker/label.py
@@ -19,6 +19,7 @@ from tilepie import tilereduce
 import label_maker
 from label_maker.utils import class_match
 from label_maker.filter import create_filter
+from label_maker.palette import class_color
 
 # declare a global accumulator so the workers will have access
 tile_results = dict()
@@ -141,7 +142,8 @@ def make_labels(dest_folder, zoom, country, classes, ml_type, bounding_box, spar
             # if we have any class pixels
             if np.sum(label):
                 label_file = '{}.png'.format(tile)
-                img = Image.fromarray(label * 255)
+                visible_label = np.array([class_color(l) for l in np.nditer(label)]).reshape(256, 256, 3)
+                img = Image.fromarray(visible_label.astype(np.uint8))
                 print('Writing {}'.format(label_file))
                 img.save(op.join(label_folder, label_file))
 
