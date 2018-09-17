7766a009b1891ebd5c97749ceb08a4aa7bf9fa8c
Drew Bollinger <drew@developmentseed.org>
2018-09-17 10:41:37 -0400

    Add toc extension

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.fulltoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
