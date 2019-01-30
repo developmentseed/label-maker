a75fcf831ac7122c9a1a0735527f41b405d077b4
William Ouellette <william.ouellette@gmail.com>
2019-01-30 12:35:35 +0100

    added aoi_name and country as list
    aoi_name is useful to give a name extension to the classification.geojson.
    country as list allows downloading of multiple countries at once.

--- label_maker/validate.py
+++ label_maker/validate.py
@@ -21,8 +21,9 @@ lat_schema = {'type': 'float', 'min': -90, 'max': 90}
 lon_schema = {'type': 'float', 'min': -180, 'max': 180}
 
 schema = {
+    'aoi_name': {'type':'string'},
     'geojson': {'type': 'string'},
-    'country': {'type': 'string', 'allowed': countries},
+    'country': {'type': 'list', 'allowed': countries},
     'bounding_box': {'type': 'list', 'items': [lon_schema, lat_schema, lon_schema, lat_schema]},
     'zoom': {'type': 'integer', 'required': True},
     'classes': {'type': 'list', 'schema': class_schema, 'required': True},
