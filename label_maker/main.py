5fa230d123bc7b25fc65f61b8d9cea4bfd7fe5f4
William Ouellette <william.ouellette@gmail.com>
2019-03-19 22:01:05 +0100

    added aoi config key

--- label_maker/main.py
+++ label_maker/main.py
@@ -82,14 +82,19 @@ def cli():
     valid = v.validate(config)
     if not valid:
         raise Exception(v.errors)
-
+        
+    # for aoi, overwrite bounding_box config key to correct labelling
+    if 'aoi' in config.keys:
+        config['bounding_box'] = get_bounds(json.load(open(config.get('aoi'), 'r')))
+        
     # custom validation for top level keys
     # require either: country & bounding_box or geojson
-    if not ('country' in config.keys() and 'geojson' in config.keys()) and not ('country' in config.keys() and 'bounding_box' in config.keys()):
-        raise Exception('either "country" and "geojson" or "country" and "bounding_box" must be present in the configuration JSON')
+    if 'geojson' not in config.keys()) and not ('country' in config.keys() and 'bounding_box' in config.keys()):
+        raise Exception('either "geojson" or "country" and "bounding_box" must be present in the configuration JSON')
 
     # for geojson, overwrite other config keys to correct labeling
     if 'geojson' in config.keys():
+        config['country'] = op.splitext(op.basename(config.get('geojson')))[0]
         config['bounding_box'] = get_bounds(json.load(open(config.get('geojson'), 'r')))
 
     if cmd == 'download':
