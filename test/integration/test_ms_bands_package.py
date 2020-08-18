609ac16374f4b98a416b10ee633aece50f59d25c
martham93 <marthamorrissey93@gmail.com>
2020-08-17 18:58:12 -0600

    test updates

--- test/integration/test_ms_bands_package.py
+++ test/integration/test_ms_bands_package.py
@@ -2,8 +2,9 @@
 label-maker package -d integration-tif/sao_tome -c test/fixtures/integration/config.intergration.geotiff_package.json"""
 import unittest
 from os import makedirs
-from shutil import copyfile, rmtree
+from shutil import copyfile, copytree, rmtree
 import subprocess
+import os
 
 import numpy as np
 
@@ -11,33 +12,35 @@ class TestClassificationPackage(unittest.TestCase):
     """Tests for local GeoTIFF package creation"""
     @classmethod
     def setUpClass(cls):
-        makedirs('integration')
-        copyfile('test/fixtures/ms-img.tif', 'integration/ms-img.tif')
-        copyfile('test/fixtures/integration/labels-ms.npz', 'integration/labels.npz')
+        makedirs('integration-ms')
+        copyfile('test/fixtures/integration/ms_img.tif', 'integration-ms/ms-img.tif')
+        copyfile('test/fixtures/integration/roads_ms.geojson', 'integration-ms/ms-roads.geojson')
+        copyfile('test/fixtures/integration/labels-ms.npz', 'integration-ms/labels.npz')
 
     @classmethod
     def tearDownClass(cls):
-        rmtree('integration')
+        rmtree('integration-ms')
 
     def test_cli(self):
         """Verify data.npz produced by CLI"""
-        cmd = 'label-maker images -d integration -c test/fixtures/integration/config.intergration.bands.json'
+        cmd = 'label-maker images --dest integration-ms --config test/fixtures/integration/config.integration.bands.json'
         cmd = cmd.split(' ')
         subprocess.run(cmd, universal_newlines=True)
 
-        cmd = 'label-maker package -d integration -c test/fixtures/integration/config.intergration.bands.json'
+        cmd = 'label-maker package --dest integration-ms --config test/fixtures/integration/config.integration.bands.json'
         cmd = cmd.split(' ')
         subprocess.run(cmd, universal_newlines=True)
 
-        data = np.load('integration/data.npz')
+        print(os.listdir('integration-ms/'))
+        data = np.load('integration-ms/data.npz')
 
-`       # validate our label data with exact matches in shape`
+        # validate our label data with exact matches in shape
         self.assertEqual(data['x_train'].shape, (12, 256, 256, 2))
         self.assertEqual(data['x_test'].shape, (4, 256, 256, 2))
 
         # validate our label data with exact matches in shape
-        self.assertEqual(data['y_train'].shape, (4, 3))
-        self.assertEqual(data['y_test'].shape, (12, 3))
+        self.assertEqual(data['y_train'].shape, (12, 3))
+        self.assertEqual(data['y_test'].shape, (4, 3))
 
         #validate img dtype
-        self.assertEqual(np.uint16, data['y_train'].dtype)
+        self.assertEqual(np.uint16, data['x_train'].dtype)
